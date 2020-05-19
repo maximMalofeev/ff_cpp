34d3f7b93ce8710a181f3c9dc3758975b14a6d60
Maxim <maximMalofeev@bk.ru>
2020-05-19 12:11:16 +0300

    fixed bug with auto firmat convertion

--- src/ff_filter.cpp
+++ src/ff_filter.cpp
@@ -31,7 +31,8 @@ struct Filter::Impl {
 };
 
 Filter::Filter(const std::string& filterDescr, int width, int height,
-               int format, const std::vector<int>& allowedFormats, bool autoConvert) {
+               int format, const std::vector<int>& allowedFormats,
+               bool autoConvert) {
   impl_ = std::make_unique<Impl>();
   impl_->filterDescription = filterDescr;
   impl_->allowedFormats = allowedFormats;
@@ -41,7 +42,9 @@ Filter::Filter(const std::string& filterDescr, int width, int height,
   UniqInOut outputs{avfilter_inout_alloc(), avFilterInOutDeleter};
   UniqInOut inputs{avfilter_inout_alloc(), avFilterInOutDeleter};
   impl_->filterGraph.reset(avfilter_graph_alloc());
-  avfilter_graph_set_auto_convert(impl_->filterGraph.get(), autoConvert);
+  avfilter_graph_set_auto_convert(
+      impl_->filterGraph.get(),
+      autoConvert ? AVFILTER_AUTO_CONVERT_ALL : AVFILTER_AUTO_CONVERT_NONE);
 
   if (!outputs || !inputs || !impl_->filterGraph) {
     throw FFCppException(av_make_error_string(AVERROR(ENOMEM)));
@@ -49,7 +52,7 @@ Filter::Filter(const std::string& filterDescr, int width, int height,
 
   // TODO: add ability customise this params
   AVRational timebase = {1, 25}, fps = {25, 1}, aspectRatio = {0, 1};
-  
+
   std::ostringstream bufFilterDescr;
   bufFilterDescr << "video_size=" << width << "x" << height
                  << ":pix_fmt=" << format << ":time_base=" << timebase.num
