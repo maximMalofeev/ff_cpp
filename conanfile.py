8e70828233bbd999bc733e68d930d0fca22d5235
Maxim <maximMalofeev@bk.ru>
2020-07-08 11:40:07 +0300

    add test package

--- conanfile.py
+++ conanfile.py
@@ -9,26 +9,17 @@ class FFCPPConan(ConanFile):
     url = "https://github.com/maximMalofeev/ff_cpp.git"
     description = "ff_cpp"
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False], "RelWithDebInfo": [True, False]}
-    default_options = {"shared": True, "RelWithDebInfo": False}
+    options = {"shared": [True, False]}
+    default_options = {"shared": False}
     generators = "cmake"
     requires = "ffmpeg/4.0@bincrafters/stable", "sdl2/2.0.9@bincrafters/stable", "catch2/2.12.2"
 
     def source(self):
        self.run("git clone https://github.com/maximMalofeev/ff_cpp.git")
-       # self.run("cd hello && git checkout static_shared")
-       # This small hack might be useful to guarantee proper /MT /MD linkage
-       # in MSVC if the packaged project doesn't have variables to set it
-       # properly
-#        tools.replace_in_file("hello/CMakeLists.txt", "PROJECT(MyHello)",
-#                              '''PROJECT(MyHello)
-# include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-# conan_basic_setup()''')
+       self.run("git checkout pkg")
 
     def build(self):
         cmake = CMake(self)
-        if self.settings.build_type=="Release" and self.options.RelWithDebInfo==True:
-            cmake.build_type = 'RelWithDebInfo'
         cmake.configure(source_folder="ff_cpp")
         cmake.build()
 
---
+++ test_package/example.cpp
@@ -0,0 +1,5 @@
+#include <ff_cpp/ff_info.h>
+
+#include <iostream>
+
+int main() { ff_cpp::dumpConfiguration(); }
